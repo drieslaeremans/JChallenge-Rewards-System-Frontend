fb1d476ebde3887fe7d1b1614a19370701b9c95b
Dries Laeremans <drieslaeremans@hotmail.com>
2018-11-19 10:39:03 +0100

    Basisconfiguratie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgBootstrapModule } from './sharedModules/ng-bootstrap.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
