2cb968f2c0cecaa802f57e81715f064f2d114169
CodingDaveS <dave.saenen@hotmail.com>
2018-11-19 16:42:17 +0100

    pulling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  readonly URL = '';
+  readonly USER_URL = '';
   constructor() { }
 
   ngOnInit() {
