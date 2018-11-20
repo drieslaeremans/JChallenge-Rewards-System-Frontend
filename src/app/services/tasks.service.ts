285aad81d2c0bc6cf9fce033c25ac8ea55ce455b
Dries Laeremans <drieslaeremans@hotmail.com>
2018-11-20 14:14:12 +0100

    Taskservice CRUD afgewerkt

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {EMPTY, Observable} from 'rxjs';
-import {catchError, share, tap} from 'rxjs/operators';
 import {TaskTemplate} from '../interfaces/task-template';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +13,8 @@ export class TasksService {
   readonly GET_TASKS_TEMPLATES = 'https://fast-temple-89292.herokuapp.com/v1/tasks/templates';
   readonly GET_TASKS_TEMPLATES_LOCAL = 'http://localhost:3000/v1/tasks/templates';
 
-  readonly GET_TASKS_TEMPLATE = 'https://fast-temple-89292.herokuapp.com/v1/tasks/template';
-  readonly GET_TASKS_TEMPLATE_LOCAL = 'http://localhost:3000/v1/tasks/template';
+  readonly GET_TASKS_TEMPLATE = 'https://fast-temple-89292.herokuapp.com/v1/task/template';
+  readonly GET_TASKS_TEMPLATE_LOCAL = 'http://localhost:3000/v1/task/template';
 
   readonly DELETE_TASK_TEMPLATE = 'https://fast-temple-89292.herokuapp.com/v1/task/template/delete/';
   readonly DELETE_TASK_TEMPLATE_LOCAL = 'http://localhost:3000/v1/task/template/delete/';
@@ -25,21 +24,30 @@ export class TasksService {
 
   constructor(private http: HttpClient) { }
 
+  // Geeft alle opdrachten (tasks) die kunnen uitgevoerd worden voor punten
   getTaskTemplates() {
-    return this.http.get(this.GET_TASKS_TEMPLATES)
+    return this.http.get(this.GET_TASKS_TEMPLATES_LOCAL)
+      .toPromise().then((tasks) => console.log(tasks));
+  }
+
+  // Geeft 1 task die uitgevoerd kan worden aan de hand van een meegegeven ID
+  getTaskTemplate(templateId) {
+    return this.http.get(this.GET_TASKS_TEMPLATE_LOCAL + '/' + templateId)
       .toPromise().then((task) => console.log(task));
   }
 
-  getTaskTemplate(templateId) {
-    return this.http.post(this.GET_TASKS_TEMPLATE, {templateId}).toPromise().then((req) => console.log(req));
+  // Voegt een task toe aan de database aan de hand van een title, points en description
+  addTaskTemplate(title: string, points: number, description: string) {
+    console.log('Voor statement');
+    this.http.post(this.ADD_TASKS_API_LOCAL,
+      {title, points, description, token: localStorage.getItem('userToken')}).toPromise()
+      .then( (message) => console.log(message.message));
+    console.log('Na statement');
+
   }
 
-  addTaskTemplate(task: TaskTemplate) {
-    this.http.post(this.ADD_TASKS_API, {title: task.title, points: task.points, description: task.description})
-      .toPromise().then((req) => console.log(req));
-  }
-
+  // Verwijderd een task uit de database aan de hand van de meegegeven ID
   deleteTaskTemplate(templateId: string) {
-    this.http.delete(this.DELETE_TASK_TEMPLATE + '/' + templateId);
+    this.http.delete(this.DELETE_TASK_TEMPLATE_LOCAL + '/' + templateId);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  APIEndPoint: 'https://fast-temple-89292.herokuapp.com/v1'
 };
 
 /*
