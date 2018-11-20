5f2f84ac1a8234622034ed3ea0f9897b7796c944
Dries Laeremans <drieslaeremans@hotmail.com>
2018-11-20 16:23:01 +0100

    RewardsService met Observables

--- src/app/services/rewards.service.ts
+++ src/app/services/rewards.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -20,31 +21,28 @@ export class RewardsService {
   readonly ADD_REWARDS_API = 'https://fast-temple-89292.herokuapp.com/v1/reward/template/add';
   readonly ADD_REWARDS_API_LOCAL = 'http://localhost:3000/v1/reward/template/add';
 
+  readonly GET_REWARDS_FEED = 'https://fast-temple-89292.herokuapp.com/v1/rewards/feed'
+
   constructor(private http: HttpClient) { }
 
-  // Geeft alle opdrachten (rewards) die kunnen uitgevoerd worden voor punten
-  getRewardTemplates() {
-    return this.http.get(this.GET_REWARDS_TEMPLATES_LOCAL)
-      .toPromise().then((rewards) => console.log(rewards));
+  getRewardTemplates(): Observable<any[]> {
+    return this.http.get<any[]>(this.GET_REWARDS_TEMPLATES_LOCAL);
   }
 
-  // Geeft 1 reward die uitgevoerd kan worden aan de hand van een meegegeven ID
-  getRewardTemplate(templateId) {
-    return this.http.get(this.GET_REWARDS_TEMPLATE_LOCAL + '/' + templateId)
-      .toPromise().then((reward) => console.log(reward));
+  getRewardTemplateById(templateId): Observable<any> {
+    return this.http.get<any>(this.GET_REWARDS_TEMPLATE + '/' + templateId);
   }
 
-  // Voegt een reward toe aan de database aan de hand van een title, points en description
+  getRewardFeed(limit = 3): Observable<any[]> {
+    return this.http.get<any[]>(this.GET_REWARDS_FEED + '/' + limit);
+  }
+
   addRewardTemplate(title: string, points: number, description: string) {
-    console.log('Voor statement');
     this.http.post(this.ADD_REWARDS_API_LOCAL,
       {title, points, description, token: localStorage.getItem('userToken')}).toPromise()
       .then( (message) => console.log(message['message']));
-    console.log('Na statement');
-
   }
 
-  // Verwijderd een reward uit de database aan de hand van de meegegeven ID
   deleteRewardTemplate(templateId: string) {
     this.http.delete(this.DELETE_REWARD_TEMPLATE_LOCAL + '/' + templateId);
   }
